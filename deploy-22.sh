a044ae2bf2abca86facbd8c7c65b37754ea856ad
Mwaka Ambrose <ambrosemwaka@gmail.com>
2023-08-25 10:50:03 +0300

    tone down the services for bare minimum app server

--- deploy-22.sh
+++ deploy-22.sh
@@ -6,49 +6,47 @@ export DEBIAN_FRONTEND=noninteractive
 [ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
 
 # Configure
-MYSQL_ROOT_PASSWORD="deploi"
-MYSQL_NORMAL_USER="deploi"
-MYSQL_NORMAL_USER_PASSWORD="deploi"
+# MYSQL_ROOT_PASSWORD="deployer"
+# MYSQL_NORMAL_USER="deployer"
+# MYSQL_NORMAL_USER_PASSWORD="deployer"
 
 # Check if password is defined
-if [[ "$MYSQL_ROOT_PASSWORD" == "" ]]; then
-    echo "${CFAILURE}Error: MYSQL_ROOT_PASSWORD not define!!${CEND}";
-    exit 1;
-fi
-if [[ "$MYSQL_NORMAL_USER_PASSWORD" == "" ]]; then
-    echo "${CFAILURE}Error: MYSQL_NORMAL_USER_PASSWORD not define!!${CEND}";
-    exit 1;
-fi
+# if [[ "$MYSQL_ROOT_PASSWORD" == "" ]]; then
+#     echo "${CFAILURE}Error: MYSQL_ROOT_PASSWORD not define!!${CEND}";
+#     exit 1;
+# fi
+# if [[ "$MYSQL_NORMAL_USER_PASSWORD" == "" ]]; then
+#     echo "${CFAILURE}Error: MYSQL_NORMAL_USER_PASSWORD not define!!${CEND}";
+#     exit 1;
+# fi
 
 # Force Locale
 
-export LC_ALL="en_US.UTF-8"
-echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
-locale-gen en_US.UTF-8
+# export LC_ALL="en_US.UTF-8"
+# echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
+# locale-gen en_US.UTF-8
 
-# Add deploi user and group, commented out 
+# Add deployer user and group, commented out 
 # becuase this user is already created in my case
 
-# addgroup deploi
-# useradd -g deploi -d /home/deploi -c "deploi data" -m -s /usr/sbin/nologin deploi
+# addgroup deployer
+echo "Adding deployer user and group"
+useradd -g deployer -d /home/deployer -c "deployer data" -m -s /usr/sbin/nologin deployer
 
 # Update Package List
-
 apt-get update
 
 # Update System Packages
-
 apt-get -y upgrade
 
 # remove apache2
 apt purge apache2 -y
 
 # Install Some PPAs
-
 apt-get install -y software-properties-common curl
 
-apt-add-repository ppa:nginx/development -y
-apt-add-repository ppa:chris-lea/redis-server -y
+# apt-add-repository ppa:nginx/development -y
+# apt-add-repository ppa:chris-lea/redis-server -y
 apt-add-repository ppa:ondrej/php -y
 
 # Using the default Ubuntu 16 MySQL 7 Build
@@ -57,30 +55,25 @@ apt-add-repository ppa:ondrej/php -y
 # sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu/ xenial mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
 
 # Install node
-curl --silent --location https://deb.nodesource.com/setup_14.x | bash -
+# curl --silent --location https://deb.nodesource.com/setup_14.x | bash -
 
 # Update Package Lists
-
 apt-get update
 
 # Install Some Basic Packages
-
 apt-get install -y build-essential dos2unix gcc git libmcrypt4 libpcre3-dev \
 make python python-pip re2c supervisor unattended-upgrades whois vim libnotify-bin
 
 # Set Kampala Timezone
-
 # ln -sf /usr/share/zoneinfo/Africa/Kampala /etc/localtime
 
 # Install PHP8.1 Stuffs
-
 apt-get install -y --force-yes php8.1-cli php8.1 \
 php8.1-gd php8.1-curl php8.1-mcrypt \
 php8.1-mysql php8.1-xdebug php8.1-mbstring php8.1-xml php8.1-zip php8.1-bcmath \
 php8.1-dom
 
 # Install Composer
-
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
@@ -88,18 +81,15 @@ mv composer.phar /usr/local/bin/composer
 printf "\nPATH=\"$(composer config -g home 2>/dev/null)/vendor/bin:\$PATH\"\n" | tee -a ~/.profile
 
 # Set Some PHP CLI Settings
-
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/8.1/cli/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/8.1/cli/php.ini
 sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/8.1/cli/php.ini
 sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.1/cli/php.ini
 
 # Install Nginx & PHP-FPM
-
 apt-get install -y --force-yes nginx php8.1-fpm
 
 # Setup Some PHP-FPM Options
-
 sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/8.1/fpm/php.ini
 sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/8.1/fpm/php.ini
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/8.1/fpm/php.ini
@@ -110,7 +100,6 @@ sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/8.1/fpm/php.ini
 sed -i "s/listen =.*/listen = 127.0.0.1:9000/" /etc/php/8.1/fpm/pool.d/www.conf
 
 # Setup Some fastcgi_params Options
-
 cat > /etc/nginx/fastcgi_params << EOF
 fastcgi_param	QUERY_STRING		\$query_string;
 fastcgi_param	REQUEST_METHOD		\$request_method;
@@ -134,66 +123,55 @@ fastcgi_param	REDIRECT_STATUS		200;
 EOF
 
 # Set The Nginx & PHP-FPM User
-
-sed -i "s/user www-data;/user deploi;/" /etc/nginx/nginx.conf
+sed -i "s/user www-data;/user deployer;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
-
-sed -i "s/user = www-data/user = deploi/" /etc/php/8.1/fpm/pool.d/www.conf
-sed -i "s/group = www-data/group = deploi/" /etc/php/8.1/fpm/pool.d/www.conf
-
-sed -i "s/listen\.owner.*/listen.owner = deploi/" /etc/php/8.1/fpm/pool.d/www.conf
-sed -i "s/listen\.group.*/listen.group = deploi/" /etc/php/8.1/fpm/pool.d/www.conf
+sed -i "s/user = www-data/user = deployer/" /etc/php/8.1/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = deployer/" /etc/php/8.1/fpm/pool.d/www.conf
+sed -i "s/listen\.owner.*/listen.owner = deployer/" /etc/php/8.1/fpm/pool.d/www.conf
+sed -i "s/listen\.group.*/listen.group = deployer/" /etc/php/8.1/fpm/pool.d/www.conf
 sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/8.1/fpm/pool.d/www.conf
 
 service nginx restart
 service php8.1-fpm restart
 
 # Install Node
-
-apt-get install -y nodejs npm
+# apt-get install -y nodejs npm
 
 # Install MySQL
-
-debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password ${MYSQL_ROOT_PASSWORD}"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password ${MYSQL_ROOT_PASSWORD}"
-apt-get install -y mysql-server
+# debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password ${MYSQL_ROOT_PASSWORD}"
+# debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password ${MYSQL_ROOT_PASSWORD}"
+# apt-get install -y mysql-server
 
 # Configure MySQL Password Lifetime
-
-echo "default_password_lifetime = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
-
-mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE USER '${MYSQL_NORMAL_USER}'@'0.0.0.0' IDENTIFIED BY '${MYSQL_NORMAL_USER_PASSWORD}';"
-mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON *.* TO '${MYSQL_NORMAL_USER}'@'127.0.0.1' IDENTIFIED BY '${MYSQL_NORMAL_USER_PASSWORD}' WITH GRANT OPTION;"
-mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON *.* TO '${MYSQL_NORMAL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_NORMAL_USER_PASSWORD}' WITH GRANT OPTION;"
-mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
-service mysql restart
+# echo "default_password_lifetime = 0" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+# mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE USER '${MYSQL_NORMAL_USER}'@'0.0.0.0' IDENTIFIED BY '${MYSQL_NORMAL_USER_PASSWORD}';"
+# mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON *.* TO '${MYSQL_NORMAL_USER}'@'127.0.0.1' IDENTIFIED BY '${MYSQL_NORMAL_USER_PASSWORD}' WITH GRANT OPTION;"
+# mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON *.* TO '${MYSQL_NORMAL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_NORMAL_USER_PASSWORD}' WITH GRANT OPTION;"
+# mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+# service mysql restart
 
 # Add Timezone Support To MySQL
-
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=${MYSQL_ROOT_PASSWORD} mysql
+# mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=${MYSQL_ROOT_PASSWORD} mysql
 
 # Install A Few Other Things
-
-apt-get install -y redis-server
+# apt-get install -y redis-server
 # apt-get install -y redis-server memcached beanstalkd
 
 # Configure Supervisor
-
 service supervisor.service enable
 service supervisor start
 
 # Enable Swap Memory
-
-# /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
-# /sbin/mkswap /var/swap.1
-# /sbin/swapon /var/swap.1
+/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
+/sbin/mkswap /var/swap.1
+/sbin/swapon /var/swap.1
 
 clear
 echo "--"
 echo "--"
 echo "It's Done."
-echo "Mysql Root Password: ${MYSQL_ROOT_PASSWORD}"
-echo "Mysql Normal User: ${MYSQL_NORMAL_USER}"
-echo "Mysql Normal User Password: ${MYSQL_NORMAL_USER_PASSWORD}"
+# echo "Mysql Root Password: ${MYSQL_ROOT_PASSWORD}"
+# echo "Mysql Normal User: ${MYSQL_NORMAL_USER}"
+# echo "Mysql Normal User Password: ${MYSQL_NORMAL_USER_PASSWORD}"
 echo "--"
 echo "--"
